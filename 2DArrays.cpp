91d5e9f05ff50095fa0ac498c3bfcdd7173aecbc
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2018-05-09 23:48:24 -0500

    Solution minus 2 test cases

---
+++ 2DArrays.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int SIZE = 6;
+
+int main(){
+  vector< vector<int> > arr(SIZE, vector<int>(SIZE));
+
+  for (int arr_i = 0; arr_i < SIZE; arr_i++){
+    for (int arr_j = 0; arr_j < SIZE; arr_j++){
+      cin >> arr[arr_i][arr_j];
+    }
+  }
+
+  int count = 0, maxCount = 0;
+  for (int arr_i = 0; arr_i < (SIZE - 2); arr_i++){
+    for (int arr_j = 0; arr_j < (SIZE - 2); arr_j++){
+      count += arr[arr_i][arr_j];
+      count += arr[arr_i][arr_j + 1];
+      count += arr[arr_i][arr_j + 2];
+      count += arr[arr_i + 1][arr_j + 1];
+      count += arr[arr_i + 2][arr_j];
+      count += arr[arr_i + 2][arr_j + 1];
+      count += arr[arr_i + 2][arr_j + 2];
+      if (count > maxCount) {
+        maxCount = count;
+      }
+      count = 0;
+    }
+  }
+
+  cout << maxCount;
+
+}
